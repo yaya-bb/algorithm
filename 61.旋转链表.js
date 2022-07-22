1a891dfd170a0e107360019fc8eec28e236aea05
yayabb <90087593+yaya-bb@users.noreply.github.com>
2022-07-22 09:39:44 +0800

    Add files via upload

---
+++ 61.旋转链表.js
@@ -0,0 +1,34 @@
+/*
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ 
+/*
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if(!head || !head.next)
+    return head;
+    let count = 0;
+    now = head;
+    while(now){
+        now = now.next;
+        count++;
+    }
+    k = k%count;
+    let slow = (fast = head);
+    while(fast.next){
+        if(k--<= 0){
+            slow = slow.next;
+        }
+        fast = fast.next;
+    }
+    fast.next = head;
+    let res = slow.next;
+    slow.next = null;
+    return res;
+};
