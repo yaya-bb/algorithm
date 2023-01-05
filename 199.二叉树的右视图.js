fbe0e2a3145e968cc25f1c28ee39953780946c87
-yayabb <2286834433@qq.com>
2023-01-05 22:40:27 +0800

    二叉树的右视图

---
+++ 199.二叉树的右视图.js
@@ -0,0 +1,18 @@
+// 层序遍历
+// 遍历到每层最后一个节点时加入res中
+var rightSideView = function(root) {
+  if(!root)
+    return [];
+  let queue = [root]; // 把根元素加入队列
+  let res = []; // 用来存储每层最后一个元素值
+  while(queue.length) {
+      let n = queue.length;
+      for(let i = 0; i < n; i++) {
+          let node =  queue.shift(); // 取出队列第一个元素
+          if(i === n - 1) res.push(node.val); // 当前一层的最后一个元素时，把值加入到res中
+          node.left && queue.push(node.left);
+          node.right && queue.push(node.right);
+      }
+  }
+  return res;
+};
