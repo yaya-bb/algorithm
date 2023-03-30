c86627f64e939db06c86b992a468604e0f4a637a
-yayabb <2286834433@qq.com>
2023-03-30 21:28:56 +0800

    二叉树的直径

---
+++ 二叉树的直径.js
@@ -0,0 +1,15 @@
+var diameterOfBinaryTree = function (root) {
+  let maxDiameter = 0;
+  const dfs = (root) => {
+    // 访问到空节点了，返回0
+    if (root == null) return 0;
+    // 左子树为根的子树的的深度
+    let leftMax = dfs(root.left);
+    let rightMax = dfs(root.right);
+    maxDiameter = Math.max(maxDiameter, leftMax + rightMax);
+    // 返回该节点为根的子树的的深度
+    return Math.max(leftMax, rightMax) + 1;
+  };
+  dfs(root);
+  return maxDiameter;
+};
